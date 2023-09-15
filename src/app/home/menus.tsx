7a28d2bba2bc7ac8025061ee1c32dd85891d0994
eyal4845 <eyal4845@gmail.com>
2023-09-16 00:46:59 +0300

    add tikshuv picture

--- src/app/home/menus.tsx
+++ src/app/home/menus.tsx
@@ -11,40 +11,49 @@ import {
 } from "./_component/icons";
 
 interface RowInMenu {
+  id: number;
   rowInfo: string;
   icon: React.ReactNode;
 }
 
 export const first_menu: RowInMenu[] = [
   {
+    id: 1,
     rowInfo: SidebarText.homePage,
     icon: <HomePageIcon />,
   },
   {
+    id: 2,
     rowInfo: SidebarText.userArea,
     icon: <PersonalAreaIcon />,
   },
   {
+    id: 3,
     rowInfo: SidebarText.courseCatalog,
     icon: <CourseCatalogIcon />,
   },
   {
+    id: 4,
     rowInfo: SidebarText.courseManager,
     icon: <CourseManagerIcon />,
   },
   {
+    id: 5,
     rowInfo: SidebarText.courseCreation,
     icon: <CourseCreationIcon />,
   },
   {
+    id: 6,
     rowInfo: SidebarText.continueStading,
     icon: <ContinueStudyingIcon />,
   },
   {
+    id: 7,
     rowInfo: SidebarText.aboutUs,
     icon: <AboutUsIcon />,
   },
   {
+    id: 8,
     rowInfo: SidebarText.logout,
     icon: <ExitIcon />,
   },
