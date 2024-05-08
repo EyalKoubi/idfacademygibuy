fc90be71d6d7f8aedf0d9bc32b6e26a08206e4dd
avishayhersh18 <avishayhersh18@gmail.com>
2024-05-08 11:34:17 +0300

    magic

--- src/app/_trpc/Provider.tsx
+++ src/app/_trpc/Provider.tsx
@@ -2,22 +2,21 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { httpBatchLink } from "@trpc/client";
 import React, { useState } from "react";
-import superjson from 'superjson'
+import superjson from "superjson";
 
 import { trpc } from "./client";
 
 export default function Provider({ children }: { children: React.ReactNode }) {
-  
   const [queryClient] = useState(() => new QueryClient({}));
   const [trpcClient] = useState(() =>
     trpc.createClient({
-      transformer:superjson,
+      transformer: superjson,
       links: [
         httpBatchLink({
           url: `${process.env.NEXT_PUBLIC_APP_URL}/api/trpc`,
         }),
       ],
-  })
+    })
   );
   return (
     <trpc.Provider client={trpcClient} queryClient={queryClient}>
@@ -25,4 +24,3 @@ export default function Provider({ children }: { children: React.ReactNode }) {
     </trpc.Provider>
   );
 }
-
