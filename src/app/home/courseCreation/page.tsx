beb9e70005a6abb51264ba0028a7996b8f570385
avishayhersh18 <avishayhersh18@gmail.com>
2024-05-09 11:10:34 +0300

    fix build error on course catalog

--- src/app/home/courseCreation/page.tsx
+++ src/app/home/courseCreation/page.tsx
@@ -1,5 +1,4 @@
-"use client"
-import "react-quill/dist/quill.snow.css";
+"use client";
 import { FormEvent, useState } from "react";
 import { AddCourseTexts, adminTexts } from "@/HebrewStrings/Texts";
 import { CourseData, ContentData } from "@/app/types";
@@ -9,8 +8,12 @@ import { useRouter } from "next/navigation";
 import useUserStore from "@/app/_contexts/userContext";
 import { Loading } from "react-daisyui";
 import ErrorMessage from "../_component/ErrorMessage";
-import ReactQuill from "react-quill";
+// import ReactQuill from "react-quill";
+import dynamic from "next/dynamic"; // Import dynamic from next/dynamic to load ReactQuill dynamically
 
+const ReactQuillNoSSR = dynamic(() => import("react-quill"), {
+  ssr: false, // Disable server-side rendering for ReactQuill
+});
 interface ErrorResponse {
   message?: string;
 }
@@ -19,18 +22,18 @@ const AddCoursePage: React.FC = () => {
   const { addCourse, initinalCourse } = useCoursesStore();
   const { addNewCourseProcess } = useUserStore();
   const [isDefaultImage, setIsDefaultImage] = useState(false);
-  const [editorValue,setEditorValue]=useState("")
+  const [editorValue, setEditorValue] = useState("");
   const [courseData, setCourseData] = useState<CourseData>({
     id: "",
     name: "",
     img_id: null,
     creationTimestamp: null,
     chapters: [],
-    subscribe_num: 0, 
-    description_sub_title: "", 
+    subscribe_num: 0,
+    description_sub_title: "",
     description: "תיאור ברירת מחדל ",
     rate: 0,
-    num_rates:0 
+    num_rates: 0,
   });
   const [fileData, setFileData] = useState<any | null>(null);
   const [error, setError] = useState<string | null>(null);
@@ -40,23 +43,25 @@ const AddCoursePage: React.FC = () => {
 
   const handleIsDefaultImageChange = (isChecked: boolean) => {
     setIsDefaultImage(isChecked);
-    
+
     if (isChecked) {
-      const defaultImageFilePath = '/default-course-image.png'
-      
+      const defaultImageFilePath = "/default-course-image.png";
+
       const reader = new FileReader();
-  
+
       reader.onload = (event) => {
         const result = event.target?.result;
-  
+
         if (result instanceof ArrayBuffer) {
-          const blob = new Blob([result], { type: 'image/png' });
-          const file = new File([blob], 'default-image-course.png', { type: 'image/png' });
-          console.log(file)
+          const blob = new Blob([result], { type: "image/png" });
+          const file = new File([blob], "default-image-course.png", {
+            type: "image/png",
+          });
+          console.log(file);
           setFileData(file);
         }
       };
-  
+
       fetch(defaultImageFilePath)
         .then((response) => response.arrayBuffer())
         .then((arrayBuffer) => {
@@ -66,9 +71,9 @@ const AddCoursePage: React.FC = () => {
       setFileData(null);
     }
   };
-  
-  const handleEditorChange = (value:string) => {
-    console.log(value)
+
+  const handleEditorChange = (value: string) => {
+    console.log(value);
     setEditorValue(value);
     setCourseData({ ...courseData, description: value }); // Update the description field in courseData
   };
@@ -87,18 +92,18 @@ const AddCoursePage: React.FC = () => {
         description_sub_title: courseData.description_sub_title,
         description: courseData.description,
         rate: courseData.rate,
-        num_rates:courseData.num_rates
+        num_rates: courseData.num_rates,
       };
-  
+
       let formData = new FormData();
-      console.log(courseData.name)
+      console.log(courseData.name);
       formData.append("course", JSON.stringify(courseToServer));
       formData.append("userId", user.id);
       formData.append("comments", courseData.name);
       formData.append("fileTitle", courseData.name);
       formData.append("file", fileData, fileData.name);
-    
-      console.log(fileData)
+
+      console.log(fileData);
       const response = await axios.post("/api/addCourse", formData, {
         headers: { "Content-Type": "multipart/form-data" },
       });
@@ -126,7 +131,9 @@ const AddCoursePage: React.FC = () => {
 
   return (
     <div className="bg-gray-50 p-6 rounded-xl shadow-lg max-w-md mx-auto mt-10">
-      <h1 className="text-3xl text-gray-800 mb-6">{adminTexts.adminAddCourse}</h1>
+      <h1 className="text-3xl text-gray-800 mb-6">
+        {adminTexts.adminAddCourse}
+      </h1>
       <input
         type="text"
         placeholder={AddCourseTexts.courseName}
@@ -134,19 +141,28 @@ const AddCoursePage: React.FC = () => {
         onChange={(e) => setCourseData({ ...courseData, name: e.target.value })}
         className="p-2 w-full border rounded-md shadow-sm mb-4"
       />
-    {/* Textarea for description_sub_title */}
-    <label className="block text-gray-600 mb-2">{AddCourseTexts.description_course_card}</label>
+      {/* Textarea for description_sub_title */}
+      <label className="block text-gray-600 mb-2">
+        {AddCourseTexts.description_course_card}
+      </label>
       <textarea
         placeholder={AddCourseTexts.description_course_card}
         value={courseData.description_sub_title}
-        onChange={(e) => setCourseData({ ...courseData, description_sub_title: e.target.value })}
+        onChange={(e) =>
+          setCourseData({
+            ...courseData,
+            description_sub_title: e.target.value,
+          })
+        }
         className="p-2 w-full border rounded-md shadow-sm mb-4"
       />
-  
-     
+
       <div>
-        <label className="block text-gray-600 mb-2">{AddCourseTexts.description_course}</label>
-        <ReactQuill
+        <label className="block text-gray-600 mb-2">
+          {AddCourseTexts.description_course}
+        </label>
+        {/* Render ReactQuill only in the browser environment */}
+        <ReactQuillNoSSR
           value={editorValue}
           onChange={handleEditorChange}
           modules={{ toolbar: true }}
@@ -190,6 +206,6 @@ const AddCoursePage: React.FC = () => {
       )}
     </div>
   );
-  };
+};
 
 export default AddCoursePage;
